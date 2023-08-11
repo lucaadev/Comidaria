a6c290df107658dc8063542ae3bad8924d631702
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-08-10 23:45:59 -0300

    FEAT: create house locations

--- web/src/pages/Cafe/index.jsx
+++ web/src/pages/Cafe/index.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+import background from '../../../src/assets/table-background.png';
+import Tables from '../../components/Tables';
+
+export default function Index() {
+
+	const { mesasCafe } = useSelector((state) => state.restaurante);
+
+	const totalPessoas = Object.values(mesasCafe).reduce((acc, mesa) => {
+		return acc + Number(mesa.quantidaDePessoas);
+	}, 0);
+
+
+	return (
+		<>
+			<div className='container-fluid d-flex flex-column align-items-center p-0'>
+				<div className='container-fluid p-0'>
+					<div className='row'>
+						<div className='col-12 d-flex flex-column justify-content-center align-items-center'>
+							<h1 className='textButton ms-3 mt-3'>Café</h1>
+							<Tables Qtdmesas={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} local={'mesasCafe'}/>
+							<h1 className='textButton mt-3'>Total: {totalPessoas} pessoas</h1>
+						</div>
+					</div>
+				</div>
+				<img src={background} alt='123' className='img-fluid backgroundStyle' />
+			</div>
+		</>
+	);
 }
--- web/src/pages/Jardim/index.jsx
+++ web/src/pages/Jardim/index.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+import background from '../../../src/assets/table-background.png';
+import Tables from '../../components/Tables';
+
+export default function Index() {
+	const { mesasJardim } = useSelector((state) => state.restaurante);
+
+	const totalPessoas = Object.values(mesasJardim).reduce((acc, mesa) => {
+		return acc + Number(mesa.quantidaDePessoas);
+	}, 0);
+
+	return (
+		<>
+			<div className='container-fluid d-flex flex-column align-items-center p-0'>
+				<div className='container-fluid p-0'>
+					<div className='row'>
+						<div className='col-12 d-flex flex-column justify-content-center align-items-center'>
+							<h1 className='textButton ms-3 mt-3'>Jardim</h1>
+							<Tables
+								Qtdmesas={[11, 12, 13, 14, 15, 16, 17, 18, 19, 20]}
+								local={'mesasJardim'}
+							/>
+							<h1 className='textButton mt-3'>Total: {totalPessoas} pessoas</h1>
+						</div>
+					</div>
+				</div>
+				<img src={background} alt='123' className='img-fluid backgroundStyle' />
+			</div>
+		</>
+	);
 }
