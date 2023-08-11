23b5ff4a71545cef416638ed52112e0e126c0eee
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-08-10 23:43:56 -0300

    FEAT: create home page

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import background from '../../../src/assets/cafe-geraes-background.png';
 
 export default function index() {
-  return (
-    <div>index</div>
-  )
+
+	return (
+		<div className='container-fluid d-flex flex-column align-items-center p-0'>
+			<div className='row'>
+				<div className='col-12 mt-5 d-flex flex-column align-items-center'>
+					<h1 className='mb-5 title'>Comidaria</h1>
+					<h2 className='text'>Escadabaixo</h2>
+					<h2 className='text'>Bar e Cozinha</h2>
+					<h2 className='text'>Café Geraes</h2>
+					<button className='mt-5 buttonStyle'>
+						<a href='/options' className='text-decoration-none textButton'>
+							Entrar
+						</a>
+					</button>
+				</div>
+			</div>
+			<img
+				src={background}
+				alt='Café Geraes'
+				className='img-fluid imageStyle'
+			/>
+		</div>
+	);
 }
