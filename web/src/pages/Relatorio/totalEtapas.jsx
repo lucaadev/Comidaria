844fd9886d51477ff6554ef7e1fa7f6555ae4c4f
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-08-12 15:35:45 -0300

    FIX: logic of showing remaining phases

--- web/src/pages/Relatorio/totalEtapas.jsx
+++ web/src/pages/Relatorio/totalEtapas.jsx
@@ -12,9 +12,36 @@ const TotalEtapas = ({ mesasCafe, mesasPub, mesasJardim, etapaIndex }) => {
 			return acc + Number(mesa.etapas[etapaIndex]);
 		}, 0);
 
+				const totalPessoasCafe = Object.values(mesasCafe).reduce(
+					(acc, mesa) => {
+						return acc + Number(mesa.quantidaDePessoas);
+					},
+					0
+				);
+
+				const totalPessoasPub = Object.values(mesasPub).reduce((acc, mesa) => {
+					return acc + Number(mesa.quantidaDePessoas);
+				}, 0);
+
+				const totalPessoasJardim = Object.values(mesasJardim).reduce(
+					(acc, mesa) => {
+						return acc + Number(mesa.quantidaDePessoas);
+					},
+					0
+				);
+
+				const total = totalPessoasCafe + totalPessoasPub + totalPessoasJardim;
+
+				const etapasRestantes = total - totalEtapas;
+
+
 	return (
 		<div className='titleRelatorio'>
-			<h1>{totalEtapas} etapas</h1>
+			<h1
+				className={`${
+					etapasRestantes < 0 ? 'text-danger' : 'text-success'
+				}`}
+			>{etapasRestantes} etapas</h1>
 		</div>
 	);
 };
